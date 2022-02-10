147ac44053a754ac0b185f997ea834352bbb3647
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2022-02-10 12:50:32 +0300

    refactor: rename class

---
+++ TennisSimulation/app/src/Models/InputData.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisSimulation.Utils;
+
+namespace TennisSimulation.Models
+{
+    public class InputData
+    {
+        public InputData() { }
+        public InputData(string fileName)
+        {
+            Console.WriteLine("reading input data from file name: " + fileName);
+            var json = TennisSimulationUtils.GetJson<InputData>(fileName);
+            PlayerModels = json.PlayerModels;
+            TournamentModels = json.TournamentModels;
+            
+            for (int i = 0; i < PlayerModels.Count; ++i)
+            {
+                PlayerModels[i].PostProcess();
+            }
+        }
+
+        [JsonProperty("players")]
+        public List<PlayerModel> PlayerModels { get; set; }
+        [JsonProperty("tournaments")]
+        public List<TournamentModel> TournamentModels { get; set; }
+    }
+}
