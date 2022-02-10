e5b7965edb6de88fef0eabd61b257d98ae845c80
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2022-02-10 12:52:32 +0300

    feat: add factory for tournament classes

---
+++ TennisSimulation/app/src/Factory/TournamentFactory.cs
@@ -0,0 +1,28 @@
+﻿using TennisSimulation.Core.Tournaments;
+using TennisSimulation.Enums;
+using TennisSimulation.Interface;
+using TennisSimulation.Models;
+using TennisSimulation.Rules;
+
+namespace TennisSimulation.Factory
+{
+    public static class TournamentFactory
+    {
+        public static Tournament GetTournament(TournamentType type, TournamentModel model)
+        {
+            switch (type)
+            {
+                case TournamentType.Elimination:
+                    return new EliminationTournament(model)
+                        .ApplyRule(new ExperienceRule(3, 0))
+                        .ApplyRule(new DominantHandRule(2, 0))
+                        .ApplyRule(new GroundTypeRule(4, 0))
+                        .ApplyRule(new ParticipationRule(1, 0));
+                case TournamentType.League:
+                    return null;
+                default:
+                    return null;
+            }
+        }
+    }
+}
