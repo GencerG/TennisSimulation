88a742291d8afd6b40308851a6ac2b4fb91041a7
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2022-02-10 12:49:47 +0300

    feat: add base abstract classes for rules and tournament types

---
+++ TennisSimulation/app/src/Abstracts/Rule.cs
@@ -0,0 +1,20 @@
+﻿using TennisSimulation.Enums;
+using TennisSimulation.Models;
+
+namespace TennisSimulation.Interface
+{
+    public abstract class Rule
+    {
+        protected int PointToLose { get; set; }
+        protected int PointToWin { get; set; }
+
+        private Rule () { }
+        public Rule (int pointToWin, int pointToLose)
+        {
+            PointToLose = pointToLose;
+            PointToWin = pointToWin;
+        }
+
+        abstract public void Execute(ref PlayerModel player1, ref PlayerModel player2, string groundType);
+    }
+}
---
+++ TennisSimulation/app/src/Abstracts/Tournament.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using TennisSimulation.Models;
+
+namespace TennisSimulation.Interface
+{
+    public abstract class Tournament
+    {
+        public Tournament (TournamentModel tournamentModel)
+        {
+            TournamentModel = tournamentModel;
+        }
+
+        private Tournament() { }
+
+        protected TournamentModel TournamentModel;
+        protected List<Rule> Rules = new List<Rule>();
+
+        public abstract void StartTournament(List<PlayerModel> participants);
+
+        public Tournament ApplyRule(Rule rule)
+        {
+            Rules.Add(rule);
+            return this;
+        }
+    }
+}
