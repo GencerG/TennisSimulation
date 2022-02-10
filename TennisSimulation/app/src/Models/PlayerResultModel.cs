b3b1a724938cf1b82c6422e8746e5d1616bdadb9
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2022-02-10 12:51:37 +0300

    feat: add new model for tournament results

---
+++ TennisSimulation/app/src/Models/PlayerResultModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisSimulation.Models
+{
+    public class PlayerResultModel
+    {
+        public int Order { get; set; }
+        public int Id { get; set; }
+        public int GainedExperience { get; set; }
+        public int TotalExperience { get; set; }
+    }
+}
