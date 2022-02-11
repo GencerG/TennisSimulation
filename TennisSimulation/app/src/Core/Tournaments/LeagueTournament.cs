9de0ed7a5cc0cd9921531eae9ca9ca78bc19c249
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2022-02-11 15:45:47 +0300

    feat: add league matchmaking system

--- TennisSimulation/app/src/Core/Tournaments/LeagueTournament.cs
+++ TennisSimulation/app/src/Core/Tournaments/LeagueTournament.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using TennisSimulation.Abstracts;
 using TennisSimulation.Models;
 
@@ -13,14 +14,40 @@ namespace TennisSimulation.Core.Tournaments
 
         public override void StartTournament(List<PlayerModel> participants)
         {
+            Console.WriteLine($"----------Starting new {TournamentModel.Type.ToUpperInvariant()} with {TournamentModel.Surface.ToUpperInvariant()} surface----------\n");
             var randomGenerator = new Random();
+            var playersWaitingForNextRound = new List<PlayerModel>();
 
-            for (int i = 0; i < participants.Count; ++i)
+            for (int i = 0; i < participants.Count - 1; ++i)
             {
-                for (int j = i + 1; j < participants.Count; ++j)
+                playersWaitingForNextRound.Clear();
+
+                for (int j = 0; j < participants.Count / 2; ++j)
                 {
-                    var player1 = participants[i];
-                    var player2 = participants[j];
+                    var availablePlayers = participants.ToList();
+
+                    for (int k = 0; k < playersWaitingForNextRound.Count; ++k)
+                    {
+                        availablePlayers.Remove(playersWaitingForNextRound[k]);
+                    }
+
+                    var player1 = availablePlayers[randomGenerator.Next(0, availablePlayers.Count)];
+                    availablePlayers.Remove(player1);
+
+                    for (int k = 0; k < player1.PlayedAgainstIds.Count; ++k)
+                    {
+                        var playedAgainstPlayer = participants.Find(player => player.Id == player1.PlayedAgainstIds[k]);
+                        availablePlayers.Remove(playedAgainstPlayer);
+                    }
+
+                    var player2 = availablePlayers[randomGenerator.Next(0, availablePlayers.Count)];
+
+                    player1.PlayedAgainstIds.Add(player2.Id);
+                    player2.PlayedAgainstIds.Add(player1.Id);
+
+                    playersWaitingForNextRound.Add(player1);
+                    playersWaitingForNextRound.Add(player2);
+
                     PlayMatch(ref player1, ref player2, randomGenerator);
                 }
             }
