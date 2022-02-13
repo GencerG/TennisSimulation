3df09ebf3649b1c82e467ef7f2bef547d3f3efc2
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2022-02-13 21:58:47 +0300

    feat: add rule and tournament configs

---
+++ TennisSimulation/app/src/Core/Tournaments/_TournamentConfigs.cs
@@ -0,0 +1,29 @@
+﻿namespace TennisSimulation.Tournaments
+{
+    public static class _TournamentConfigs
+    {
+        public readonly struct ELIMINATION
+        {
+            public readonly struct REWARDS
+            {
+                public readonly struct EXPERIENCE
+                {
+                    public const int WINNER_PRIZE = 20;
+                    public const int LOSER_PRIZE = 10;
+                }
+            }
+        }
+
+        public readonly struct LEAGUE
+        {
+            public readonly struct REWARDS
+            {
+                public readonly struct EXPERIENCE
+                {
+                    public const int WINNER_PRIZE = 10;
+                    public const int LOSER_PRIZE = 1;
+                }
+            }
+        }
+    }
+}
